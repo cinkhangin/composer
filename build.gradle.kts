363828b8c700ccfa607620858cf2a7c83523086d
Cin Khan Gin <62807678+cinkhangin@users.noreply.github.com>
2026-07-03 06:52:07 +0800

    upgrade to Kotlin 2.4.0; re-enable wasm incremental compilation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Root project declares plugin versions only; modules apply what they need.
 // This keeps `model` and `codegen` free of the Compose plugin entirely.
 plugins {
-    kotlin("multiplatform") version "2.3.20" apply false
-    kotlin("plugin.serialization") version "2.3.20" apply false
+    kotlin("multiplatform") version "2.4.0" apply false
+    kotlin("plugin.serialization") version "2.4.0" apply false
     id("org.jetbrains.compose") version "1.11.1" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.0" apply false
 }
